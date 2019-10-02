b9fdc4591c37d215a9316d3fb102b6733ebdbe78
Rohan Kakar <rkakar2000@gmail.com>
2019-10-02 16:44:19 +0530

    IT FINALLY WORKS!!!!

--- scripts.js
+++ scripts.js
@@ -1,15 +1,46 @@
-const container = document.querySelector("#container");
-for(var x=0;x<16;x++){
-  for(var y=0;y<16;y++){
-    var div = $("<div class='unit'></div>");
-    div.appendTo(container);
+var container = document.querySelector("#container");
+var input = document.querySelector("#order");
+var button = document.querySelector("#resetButton");
+var bluestuff = [];
+var setButton = document.querySelector("#set");
+setButton.addEventListener("click",createStuff);
+function createStuff(){
+  deleteChild(container);
+  var order = input.value;
+  var width = 960/order;
+  var height = width + "px";
+  for(var x=0;x<order;x++){
+  for(var y=0;y<order;y++){
+    var div = document.createElement('div');
+    div.style.backgroundColor = "red";
+    div.style.height = height;
+    div.style.width = height;
+    div.style.float = "left";
+    div.style.borderStyle = "solid";
+    div.classList.add("unit");
+    container.appendChild(div);
+    }
+  }
+  var divs = document.getElementsByClassName("unit");
+  for(var x of divs){  
+    x.addEventListener("mouseenter",function(event){
+    event.target.style.backgroundColor = 'blue';
+    addtoArray(event.target);
+    });
   }
 }
-
-var divs = document.querySelectorAll(".unit");
-
-for(var x in divs){
-  divs[x].addEventListener("mouseenter",function(event){
-    event.target.style.backgroundColor = "blue";
-  });
+function addtoArray(target){
+  bluestuff.push(target);
 }
+button.addEventListener("click",function(){
+  while(bluestuff){
+    bluestuff.pop().style.backgroundColor = "red";
+  }
+});
+function deleteChild(e) { 
+  var child = e.lastElementChild;  
+  while (child) { 
+    e.removeChild(child); 
+    child = e.lastElementChild; 
+  } 
+} 
